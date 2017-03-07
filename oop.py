21a01677bd55cb334dbdad9b8f7055121ad6c053
WNjihia <dorisn172@gmail.com>
2017-03-07 17:47:39 +0300

    add docstring

--- oop.py
+++ oop.py
@@ -1,17 +1,24 @@
 class Shape(object):
+    """ Creates superclass Shape"""
     def __init__(self, length=0, width=0):
-        self.__length = length
-        self.__width = width
+        self.length = length
+        self.width = width
 
     def getarea(self):
-        print (0)
+        return 0
 
 
 class Square(Shape):
+    """Square is a subclass of Shape"""
     def getarea(self, length, width):
-        print (length*width)
+        print(length * width)
 
 
 class Triangle(Shape):
+    """Triangle is a subclass of Shape"""
     def getarea(self, length, width):
-        print (0.5*(length*width))
+        print(0.5 * (length*width))
+
+
+Square(4, 4)
+Triangle(4, 4)
